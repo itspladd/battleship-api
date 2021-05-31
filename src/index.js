57539d24c91391ff024a6363327e5a7648a9a73d
Paul Ladd <vlendle@gmail.com>
2021-05-31 10:36:05 -0500

    Add initial route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
 console.log("Server running!")
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/backend-test', (req, res) => {
+  res.send({ msg: 'Successful connection!' })
+});
