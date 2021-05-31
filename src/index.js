3eb5622e2e9ab539c2ab83058d9cc02aa632b5c1
Paul Ladd <vlendle@gmail.com>
2021-05-31 14:13:28 -0500

    Add db dependencies and basic seeds

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ console.log("Server running!")
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-db.query(`DROP TABLE IF EXISTS test; CREATE TABLE test (name VARCHAR(255)); INSERT INTO test VALUES ('dave'); SELECT * FROM test;`, [])
-  .then(res => console.log(res[3]))
-  .catch(err => console.error(err))
-
 app.get('/', (req, res) => {
   res.json({ you: "are on the root page"})
 });
