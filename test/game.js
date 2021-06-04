f7995b269ef346469d219a243dcb8a2c0a7e0f17
Paul Ladd <vlendle@gmail.com>
2021-06-04 15:16:33 -0500

    Add initial game engine test and remove boilerplate test file

---
+++ test/game.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe('Game Engine', () => {
+  describe('Game()', () => {
+    it('should create an instance of a game engine');
+    it('should contain an array of the players');
+    it('should contain an array of the moves taken');
+  })
+})
