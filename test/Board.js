dc3336ac3288e1e33ef707f27aedb11ce08652bf
Paul Ladd <vlendle@gmail.com>
2021-06-05 09:18:50 -0500

    Add test for Board size

--- test/Board.js
+++ test/Board.js
@@ -10,5 +10,9 @@ describe('Board', () => {
     it('should create an instance of a Board', () => {
       should.exist(testBoard)
     });
+    it('should default to a size of 10 by 10', () => {
+      testBoard.rows.should.equal(10);
+      testBoard.columns.should.equal(10);
+    })
   })
 })
