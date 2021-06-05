5ffc9e95bb4702f2bb848c80a3794d9ad59ff1f2
Paul Ladd <vlendle@gmail.com>
2021-06-05 15:03:18 -0500

    Add getNeighbor implementation that passes tests to get neighboring tiles

--- src/classes/Board.js
+++ src/classes/Board.js
@@ -81,6 +81,57 @@ class Board {
         position: ${position}
         maxPosition: ${this.maxPosition}`);
     }
+    const x = position[0];
+    const y = position[1];
+    let neighborPosition;
+
+    if (x % 2 === 1) {
+      // Case where x is odd
+      switch (angle) {
+        case 0:
+          neighborPosition = [x, y - 1];
+          break;
+        case 60:
+          neighborPosition = [x + 1, y];
+          break;
+        case 120:
+          neighborPosition = [x + 1, y + 1];
+          break;
+        case 180:
+          neighborPosition = [x, y + 1];
+          break;
+        case 240:
+          neighborPosition = [x - 1, y + 1];
+          break;
+        case 300:
+          neighborPosition = [x - 1, y];
+          break;
+      }
+    } else if (x % 2 === 0) {
+      // Case where x is even
+      switch (angle) {
+        case 0:
+          neighborPosition = [x, y - 1];
+          break;
+        case 60:
+          neighborPosition = [x + 1, y - 1];
+          break;
+        case 120:
+          neighborPosition = [x + 1, y];
+          break;
+        case 180:
+          neighborPosition = [x, y + 1];
+          break;
+        case 240:
+          neighborPosition = [x - 1, y];
+          break;
+        case 300:
+          neighborPosition = [x - 1, y - 1];
+          break;
+      }
+    }
+
+    return this.positionIsInsideBoard(neighborPosition) ? neighborPosition : null;
   }
 }
 
