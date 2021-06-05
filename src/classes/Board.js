09fe8d9b2672c878eead1464672b1f8c2eb85136
Paul Ladd <vlendle@gmail.com>
2021-06-05 09:37:54 -0500

    Add test and implementation for filling Board with TIle objects at construction

--- src/classes/Board.js
+++ src/classes/Board.js
@@ -1,8 +1,23 @@
+const Tile = require("./Tile");
 
 class Board {
   constructor() {
     this.rows = 10;
     this.columns = 10;
+
+    this.tiles= this.initTiles(this.rows, this.columns);
+  }
+
+  initTiles(rows, columns) {
+    const result = []
+    for(let i = 0; i < rows; i++) {
+      const row = [];
+      for(let j = 0; j < columns; j++) {
+        row.push(new Tile());
+      }
+      result.push(row)
+    }
+    return result;
   }
 }
 
--- test/Board.js
+++ test/Board.js
@@ -1,5 +1,6 @@
 const should = require('chai').should()
 const Board = require('../src/classes/Board')
+const Tile = require('../src/classes/Tile')
 
 describe('Board', () => {
   describe('Board()', () => {
@@ -15,7 +16,14 @@ describe('Board', () => {
       testBoard.columns.should.equal(10);
     })
     it('should be full of Tile objects', () => {
-      
+      const rows = testBoard.tiles.length;
+      const cols = testBoard.tiles[0].length;
+      testBoard.tiles[0][0].should.be.an.instanceof(Tile);
+      testBoard.tiles[rows / 2][cols / 2].should.be.an.instanceof(Tile);
+      testBoard.tiles[rows - 1][cols - 1].should.be.an.instanceof(Tile);
+    })
+    it('should be able to change one Tile object without changing others', () => {
+
     })
   })
 })
