e3f9e4476b977d9eabeff1b917fddfadfa70171f
Paul Ladd <vlendle@gmail.com>
2021-06-14 09:19:19 -0500

    Fix errors in noDuplicateUnderscoresRecursive test that forced it into pending, and fix errors causing the test to fail

--- test/00_generalHelpers_test.js
+++ test/00_generalHelpers_test.js
@@ -1,3 +1,5 @@
+const should = require('chai').should()
+
 const { 
   shuffleArray,
   noDuplicateUnderscoresRecursive,
@@ -5,16 +7,18 @@ const {
 } = require('../src/helpers/generalHelpers')
 
 describe('noDuplicateUnderscoresRecursive()', () => {
-  it('should detect underscores in nested objects'), () => {
+  it('should detect underscores in nested objects', () => {
     const badObj1 = { test: 1, _test: 3 };
     const badObj2 = { test: 1, test2: { a: { a: 1, _a: 2 }}};
     const goodObj1 = { a: 1, b: { _a: 3, _b: 2 } };
-    const goodObj2 = { a: 1, b: { a: 3, b: { b: 1, _b: 2 }}}
-    noDuplicateUnderscoresRecursive(badObj1).should.be.false;
-    noDuplicateUnderscoresRecursive(badObj2).should.be.true;
+    const goodObj2 = { a: 1, b: { a: 3, b: { b: 1, _a: 2 }}};
+    const bad1 = () => noDuplicateUnderscoresRecursive(badObj1)
+    const bad2 = () => noDuplicateUnderscoresRecursive(badObj2)
+    bad1.should.throw(Error);
+    bad2.should.throw(Error);
     noDuplicateUnderscoresRecursive(goodObj1).should.be.true;
     noDuplicateUnderscoresRecursive(goodObj2).should.be.true;
-  }
+  });
 })
 
 describe('stripUnderscoresRecursive()', () => {
